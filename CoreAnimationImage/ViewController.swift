b6369fc85bc75381d7b53e26e153b837c9b9d5aa
Miklos Kekkoi <focusswiftconnect@gmail.com>
2017-11-25 16:36:14 +0100

    Initial Commit

---
+++ CoreAnimationImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreAnimationImage
+//
+//  Created by Miklos Kekkoi on 11/25/17.
+//  Copyright © 2017 Miklos Kekkoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
